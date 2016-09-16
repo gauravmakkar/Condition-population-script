98c197601e922b8c90e25ff24ada60bc03b01fe6
Gaurav MphRx <gmakkar119@gmail.com>
2016-09-16 13:29:39 +0530

    removed loggings

--- script.js
+++ script.js
@@ -37,8 +37,7 @@ mongoClient.connect(url,function(err,db){
                 //Example
 				var code= arr[0].substr(0,arr[0].indexOf(" "))
 				var text=arr[0].substr(arr[0].indexOf(" "),arr[0].length).trim()
-                console.log({code:code,text:text,description:text,system:"ICD10"})
-				bulk.insert({code:code,text:text,description:text,system:"ICD10"});
+                bulk.insert({code:code,text:text,description:text,system:"ICD10"});
             }
             catch (err){
                 console.log(err);
